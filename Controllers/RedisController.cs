fde00b25062e90edc1f48bc81cd3f4e74b73904c
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2022-07-22 09:06:42 +0300

    Added date cache method.

--- Controllers/RedisController.cs
+++ Controllers/RedisController.cs
@@ -9,10 +9,16 @@ namespace Redis.Controllers
     public class RedisController : ControllerBase
     {
         private readonly IConnectionMultiplexer _redis;
+        
+        private readonly IDistributedCache _cache;
 
-        public RedisController(IConnectionMultiplexer redis)
+
+        public RedisController(
+            IConnectionMultiplexer redis, 
+            IDistributedCache _cache)
         {
             _redis = redis;
+            _cache = cache;
         }
 
         [HttpGet("test")]
@@ -21,6 +27,27 @@ namespace Redis.Controllers
             var db = _redis.GetDatabase();
             var foo = await db.StringGetAsync("test");
             return Ok(foo.ToString());
+        }
+        
+        [HttpGet("date")]
+        public async Task<IActionResult> Date()
+        {
+            DateTime dateTime = DateTime.Now;
+            var cachedData = await _cache.GetStringAsync("cachedTime");
+
+            if (cachedData == null)
+            {
+                cachedData = dateTime.ToString();
+                DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions()
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(20),
+                    AbsoluteExpiration = DateTime.Now.AddSeconds(60)
+                };
+
+                await _cache.SetStringAsync("cachedTime", cachedData, cacheOptions);
+            }
+
+            return View(nameof(Index), cachedData);
         }
     }
 }
